07ae3a7404a68d67cddcc88a7d47f0650c997e97
danishjamadar5 <117918682+danishjamadar5@users.noreply.github.com>
2023-06-14 18:36:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the body element
+    var body = document.body;
+
+    // Check if the body is empty
+    if (body.childElementCount === 0) {
+        // Create a new text node with the message
+        var textNode = document.createTextNode('DOM load success');
+
+        // Append the text node to the body
+        body.appendChild(textNode);
+    }
+});
